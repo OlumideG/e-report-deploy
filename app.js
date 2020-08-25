e510b7450e869ebe037e3b1b8b04580f200e349c
OlumideG <olumidegbenjo@gmail.com>
2020-08-25 10:33:03 +0100

    get request

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use( (req,res, next) =>{
 // })
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 if (app.get("env") === "development"){
